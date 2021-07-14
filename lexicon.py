f50a752f893d3a6b1ea9bb94b4bf9ad1702e2dcd
MackieZhou <mzhou@smith.edu>
2021-07-15 00:04:16 +0800

    sample lexicon 07/14/2021

--- lexicon.py
+++ lexicon.py
@@ -1,19 +1,24 @@
 from analyzer import Request, Analyzer
-from mental_model import Mental_model, Time_step
-from map import Containment, Touching, Space
 
 # lexicon should be a hash table (python dictionary)
 # key - the word
 # value - definition of the word (a list of request objects)
-lex = {}
-lex["JACK"] = [Request(test=True, text="noun JACK", assign=None, next_packet=None)]
-lex["AND"] = [Request(test=True, text="conjunction AND", assign=None, next_packet=None)]
-lex["SUSAN"] = [Request(test=True, text="noun SUSAN", assign=None, next_packet=None)]
-lex["HAVE"] = [Request(test=True, text="verb HAVE", assign=None, next_packet=None)]
-lex["A"] = [Request(test=True, text="particle A", assign=None, next_packet=None)]
-lex["BALL"] = [Request(test=True, text="noun BALL", assign=None, next_packet=None)]
+lex = dict()
+lex["*START*"] = [Request(text="start parsing", test=True,
+                          next_packet=[Request(text="part-of-speech is noun-phrase",
+                                               test_val={"part-of-speech": "noun-phrase"},
+                                               next_packet=[Request(text="part-of-speech is verb",
+                                                                    test_val={"part-of-speech": "verb"})])])]
+
+lex["JACK"] = [
+    Request(text="noun JACK", test=True, test_val=None, assign_val={"part-of-speech": "noun-phrase"}, next_packet=None)]
+lex["AND"] = [Request(text="conjunction AND", test=True, test_val=None, assign_val=None, next_packet=None)]
+lex["SUSAN"] = [Request(text="noun SUSAN", test=True, test_val=None, assign_val=None, next_packet=None)]
+lex["HAVE"] = [
+    Request(text="verb HAVE", test=True, test_val=None, assign_val={"part-of-speech": "verb"}, next_packet=None)]
+lex["A"] = [Request(text="particle A", test=True, test_val=None, assign_val=None, next_packet=None)]
+lex["BALL"] = [Request(text="noun BALL", test=True, test_val=None, assign_val=None, next_packet=None)]
 
 analyzer = Analyzer(lexicon=lex)
 
 print(analyzer.parse("   !!!  Jack And    susan   HAVE a baLL   !!.;  ; ... ,"))
-# print(analyzer.SENTENCE)
