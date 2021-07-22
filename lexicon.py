2f11b70a7b8f576fb66ab5c5bc980f2f665c2e81
MackieZhou <mzhou@smith.edu>
2021-07-22 20:48:30 +0800

    sample lexicon 07/22/2021 (plants absorb water)

--- lexicon.py
+++ lexicon.py
@@ -1,24 +1,37 @@
 from analyzer import Request, Analyzer
+from map import Containment, Space, Touching
+from mental_model import Time_step, Mental_model
 
 # lexicon should be a hash table (python dictionary)
 # key - the word
 # value - definition of the word (a list of request objects)
 lex = dict()
-lex["*START*"] = [Request(text="start parsing", test=True,
-                          next_packet=[Request(text="part-of-speech is noun-phrase",
-                                               test_val={"part-of-speech": "noun-phrase"},
-                                               next_packet=[Request(text="part-of-speech is verb",
-                                                                    test_val={"part-of-speech": "verb"})])])]
+lex["*START*"] = [Request(text="start parsing",
+                          test_flag=True,
+                          next_packet=[Request(text="PART-OF-SPEECH is noun-phrase",
+                                               tests={"PART-OF-SPEECH": "noun-phrase"},
+                                               assigns={"SUBJECT": "*CD"},
+                                               next_packet=[Request(text="PART-OF-SPEECH is verb",
+                                                                    tests={"PART-OF-SPEECH": "verb"},
+                                                                    assigns={"CONCEPT": "*CD"})])])]
 
-lex["JACK"] = [
-    Request(text="noun JACK", test=True, test_val=None, assign_val={"part-of-speech": "noun-phrase"}, next_packet=None)]
-lex["AND"] = [Request(text="conjunction AND", test=True, test_val=None, assign_val=None, next_packet=None)]
-lex["SUSAN"] = [Request(text="noun SUSAN", test=True, test_val=None, assign_val=None, next_packet=None)]
-lex["HAVE"] = [
-    Request(text="verb HAVE", test=True, test_val=None, assign_val={"part-of-speech": "verb"}, next_packet=None)]
-lex["A"] = [Request(text="particle A", test=True, test_val=None, assign_val=None, next_packet=None)]
-lex["BALL"] = [Request(text="noun BALL", test=True, test_val=None, assign_val=None, next_packet=None)]
+# lex["JACK"] = [
+#     Request(text="noun JACK", test=True, test_val=None, assign_val={"PART-OF-SPEECH": "noun-phrase"}, next_packet=None)]
+# lex["AND"] = [Request(text="conjunction AND", test=True, test_val=None, assign_val=None, next_packet=None)]
+# lex["SUSAN"] = [Request(text="noun SUSAN", test=True, test_val=None, assign_val=None, next_packet=None)]
+# lex["HAVE"] = [
+#     Request(text="verb HAVE", test=True, test_val=None, assign_val={"PART-OF-SPEECH": "verb"}, next_packet=None)]
+# lex["A"] = [Request(text="particle A", test=True, test_val=None, assign_val=None, next_packet=None)]
+# lex["BALL"] = [Request(text="noun BALL", test=True, test_val=None, assign_val=None, next_packet=None)]
+
+lex["PLANTS"] = [Request(text="noun PLANTS", test_flag=True, assigns={"CD": "PLANTS", "PART-OF-SPEECH": "noun-phrase"})]
+lex["ABSORB"] = [Request(text="verb ABSORB", test_flag=True, assigns={"CD": "ABSORB", "PART-OF-SPEECH": "verb"},
+                         next_packet=[Request(text="PART-OF-SPEECH is noun-phrase",
+                                              tests={"PART-OF-SPEECH": "noun-phrase"},
+                                              calls="CONTAIN")])]
+lex["WATER"] = [Request(text="noun WATER", test_flag=True, assigns={"CD": "WATER", "PART-OF-SPEECH": "noun-phrase"})]
 
 analyzer = Analyzer(lexicon=lex)
 
-print(analyzer.parse("   !!!  Jack And    susan   HAVE a baLL   !!.;  ; ... ,"))
+# print(analyzer.parse("   !!!  Jack And    susan   HAVE a baLL   !!.;  ; ... ,"))
+print(analyzer.parse("plants absorb water"))
