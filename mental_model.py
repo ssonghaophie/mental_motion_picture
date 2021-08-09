a52344cb4ce3a519226b831f8456fd1afe462859
MackieZhou <mzhou@smith.edu>
2021-08-09 20:43:11 +0800

    add a pointer that points to the latest time-step. add a updateACT() method.

--- mental_model.py
+++ mental_model.py
@@ -1,4 +1,5 @@
 from map import Containment, Space, Touching
+from copy import deepcopy
 
 """
 The Mental_map class is adapted from the linked_list class from
@@ -18,74 +19,43 @@ class Time_step:
 
 
 class Mental_model:
-    def __init__(self, containment, space, touching):
-        self.head = Time_step(containment, space, touching)
+    def __init__(self):
+        self.head = Time_step(Containment(), Space(), Touching())
+        self.count = 1  # number of time_steps in this model
+        self.cur = self.head  # the latest node
 
     def advance_time(self):
         """copy the current Time_step to a new Time_step"""
-        cur = self.head
-        while cur.next is not None:
-            cur = cur.next
-        new_node = Time_step(cur.containment.copy(),
-                             cur.space.copy(), cur.touching.copy())
+        self.count += 1
+        new_node = Time_step(self.cur.containment.copy(),
+                             self.cur.space.copy(),
+                             self.cur.touching.copy())
 
         # copy PTRANS actions to the new time_step
-        if len(cur.action["PTRANS"]) > 0:
-            new_node.action["PTRANS"] = cur.action["PTRANS"].copy()
+        if len(self.cur.action["PTRANS"]) > 0:
+            new_node.action["PTRANS"] = deepcopy(self.cur.action["PTRANS"])
 
-        if len(cur.action["PSTOP"]) > 0:
-            for action_s in cur.action["PSTOP"]:
+        if len(self.cur.action["PSTOP"]) > 0:
+            for action_s in self.cur.action["PSTOP"]:
                 for action_t in new_node.action["PTRANS"]:
                     if action_t["object"] == action_s["object"]:
                         new_node.action["PTRANS"].remove(action_t)
                         break
-        cur.next = new_node
-
-    # def append(self, containment, space):
-    #     """add a new Time_step"""
-    #     new_node = Time_step(containment, space)
-    #     cur = self.head
-    #     while cur.next is not None:
-    #         cur = cur.next
-    #     cur.next = new_node
-
-    def length(self):
-        """return the total number of Time_steps"""
-        cur = self.head
-        total = 1
-        while cur.next is not None:
-            total += 1
-            cur = cur.next
-        return total
-
-    # def display(self):
-    #     """print the whole linked_list"""
-    #     elems = []
-    #     cur_node = self.head
-    #     while cur_node.next is not None:
-    #         cur_node = cur_node.next
-    #         elems.append(cur_node.data)
-    #     print(elems)
+        self.cur.next = new_node
+        self.cur = self.cur.next
 
     def get(self, index):
         """Returns the value of the Time_step at a certain index"""
-        if index >= self.length() or index < 0:
+        if index > self.count or index < 1:
             print("ERROR: Index out of range!")
             return None
-        cur_idx = 0
+        cur_idx = 1
         cur_node = self.head
         while True:
             if cur_idx == index:
                 return cur_node
             cur_node = cur_node.next
             cur_idx += 1
-
-    def get_current(self):
-        """Return the current/last Time_step"""
-        cur = self.head
-        while cur.next is not None:
-            cur = cur.next
-        return cur
 
     def get_containment(self, index):
         """Returns the containment map of the time_stamp at index"""
@@ -101,74 +71,61 @@ class Mental_model:
 
     def add_object(self, object):
         """add object to all maps of the last time_step"""
-        cur = self.get_current()
-        cur.containment.add_object(object)
-        cur.space.add_object(object)
-        cur.touching.add_object(object)
+        self.cur.containment.add_object(object)
+        self.cur.space.add_object(object)
+        self.cur.touching.add_object(object)
 
     def contain(self, edge):
         """add an edge to the containment map of the last time_step"""
-        cur = self.get_current()
-
         for object in edge:
-            if object not in cur.containment._graph_dict:
-                cur.containment.add_object(object)
-                cur.space.add_object(object)
-                cur.touching.add_object(object)
-        cur.containment.contain(edge)
+            if object not in self.cur.containment._graph_dict:
+                self.cur.containment.add_object(object)
+                self.cur.space.add_object(object)
+                self.cur.touching.add_object(object)
+        self.cur.containment.contain(edge)
 
     def x_contain(self, edge):
         """remove an edge from the containment map of the last time_step"""
-        cur = self.get_current()
-        cur.containment.x_contain(edge)
+        self.cur.containment.x_contain(edge)
 
     def above(self, edge):
         """add an edge to the space map of the last time_step"""
-        cur = self.get_current()
-
         for object in edge:
-            if object not in cur.containment._graph_dict:
-                cur.containment.add_object(object)
-                cur.space.add_object(object)
-                cur.touching.add_object(object)
-        cur.space.above(edge)
+            if object not in self.cur.containment._graph_dict:
+                self.cur.containment.add_object(object)
+                self.cur.space.add_object(object)
+                self.cur.touching.add_object(object)
+        self.cur.space.above(edge)
 
     def x_above(self, edge):
         """remove an edge from the space map of the last time_step"""
-        cur = self.get_current()
-        cur.space.x_above(edge)
+        self.cur.space.x_above(edge)
 
     def under(self, edge):
         """add an edge to the space map of the last time_step"""
-        cur = self.get_current()
-
         for object in edge:
-            if object not in cur.containment._graph_dict:
-                cur.containment.add_object(object)
-                cur.space.add_object(object)
-                cur.touching.add_object(object)
-        cur.space.under(edge)
+            if object not in self.cur.containment._graph_dict:
+                self.cur.containment.add_object(object)
+                self.cur.space.add_object(object)
+                self.cur.touching.add_object(object)
+        self.cur.space.under(edge)
 
     def x_under(self, edge):
         """remove an edge from the space map of the last time_step"""
-        cur = self.get_current()
-        cur.space.x_under(edge)
+        self.cur.space.x_under(edge)
 
     def touch(self, edge):
         """add an edge to the touching map of the last time_step"""
-        cur = self.get_current()
-
         for object in edge:
-            if object not in cur.containment._graph_dict:
-                cur.containment.add_object(object)
-                cur.space.add_object(object)
-                cur.touching.add_object(object)
-        cur.touching.touch(edge)
+            if object not in self.cur.containment._graph_dict:
+                self.cur.containment.add_object(object)
+                self.cur.space.add_object(object)
+                self.cur.touching.add_object(object)
+        self.cur.touching.touch(edge)
 
     def x_touch(self, edge):
         """remove an edge from the touching map of the last time_step"""
-        cur = self.get_current()
-        cur.touching.x_touch(edge)
+        self.cur.touching.x_touch(edge)
 
     def print(self, index):
         """ print the containment map and space map
@@ -176,113 +133,101 @@ class Mental_model:
         """
         time_step = self.get(index)
 
-        print("\nCONTAINMENT RELATIONSHIP ---------------------")
+        print("\nCONTAINMENT RELATIONSHIP -------------------------------")
         print(time_step.containment)
 
-        print("\nSPATIAL RELATIONSHIP -------------------------")
+        print("\nSPATIAL RELATIONSHIP -----------------------------------")
         print(time_step.space)
 
-        print("\nTOUCHING RELATIONSHIP ------------------------")
+        print("\nTOUCHING RELATIONSHIP ----------------------------------")
         print(time_step.touching)
 
-        print("\nPRIMITIVE ACTIONS ----------------------------")
+        print("\nPRIMITIVE ACTIONS --------------------------------------")
         for action in time_step.action:
             print(action, time_step.action[action])
 
     def print_latest(self):
-        time_step = self.get_current()
-
-        print("\nCONTAINMENT RELATIONSHIP ---------------------")
-        print(time_step.containment)
-
-        print("\nSPATIAL RELATIONSHIP -------------------------")
-        print(time_step.space)
-
-        print("\nTOUCHING RELATIONSHIP ------------------------")
-        print(time_step.touching)
-
-        print("\nPRIMITIVE ACTIONS ----------------------------")
-        for action in time_step.action:
-            print(action, time_step.action[action])
+        self.print(self.count)
 
     def __getitem__(self, index):
         """Allows for bracket operator syntax (i.e. a[0] = first item)"""
         return self.get(index)
 
     def PTRANS(self, object, to=None, From=None):
-        cur = self.get_current()
-
-        if object not in cur.containment._graph_dict:
-            cur.containment.add_object(object)
-            cur.space.add_object(object)
-            cur.touching.add_object(object)
+        if object not in self.cur.containment._graph_dict:
+            self.cur.containment.add_object(object)
+            self.cur.space.add_object(object)
+            self.cur.touching.add_object(object)
 
         # new_dict = ["object " + object, "from " + From, "to " + to]
         new_dict = {"object": object, "from": From, "to": to}
-        cur.action["PTRANS"].append(new_dict)
+        self.cur.action["PTRANS"].append(new_dict)
 
     def PSTOP(self, object):
-        cur = self.get_current()
+        if object not in self.cur.containment._graph_dict:
+            self.cur.containment.add_object(object)
+            self.cur.space.add_object(object)
+            self.cur.touching.add_object(object)
 
-        if object not in cur.containment._graph_dict:
-            cur.containment.add_object(object)
-            cur.space.add_object(object)
-            cur.touching.add_object(object)
-
-        cur.action["PSTOP"].append({})
-        cur.action["PSTOP"][-1]["object"] = object
+        self.cur.action["PSTOP"].append({})
+        self.cur.action["PSTOP"][-1]["object"] = object
 
     def INGEST(self, object, container):
-        cur = self.get_current()
-
         for thing in [object, container]:
-            if thing not in cur.containment._graph_dict:
-                cur.containment.add_object(thing)
-                cur.space.add_object(thing)
-                cur.touching.add_object(thing)
+            if thing not in self.cur.containment._graph_dict:
+                self.cur.containment.add_object(thing)
+                self.cur.space.add_object(thing)
+                self.cur.touching.add_object(thing)
 
-        cur.action["INGEST"].append({})
-        cur.action["INGEST"][-1]["object"] = object
-        cur.action["INGEST"][-1]["container"] = container
+        self.cur.action["INGEST"].append({})
+        self.cur.action["INGEST"][-1]["object"] = object
+        self.cur.action["INGEST"][-1]["container"] = container
 
     def EXPEL(self, object, container):
-        cur = self.get_current()
-
         for thing in [object, container]:
-            if thing not in cur.containment._graph_dict:
-                cur.containment.add_object(thing)
-                cur.space.add_object(thing)
-                cur.touching.add_object(thing)
+            if thing not in self.cur.containment._graph_dict:
+                self.cur.containment.add_object(thing)
+                self.cur.space.add_object(thing)
+                self.cur.touching.add_object(thing)
 
-        cur.action["EXPEL"].append({})
-        cur.action["EXPEL"][-1]["object"] = object
-        cur.action["EXPEL"][-1]["container"] = container
+        self.cur.action["EXPEL"].append({})
+        self.cur.action["EXPEL"][-1]["object"] = object
+        self.cur.action["EXPEL"][-1]["container"] = container
 
     def STATECHANGE(self, object, to):
-        cur = self.get_current()
-
         for thing in [object, to]:
-            if thing not in cur.containment._graph_dict:
-                cur.containment.add_object(thing)
-                cur.space.add_object(thing)
-                cur.touching.add_object(thing)
+            if thing not in self.cur.containment._graph_dict:
+                self.cur.containment.add_object(thing)
+                self.cur.space.add_object(thing)
+                self.cur.touching.add_object(thing)
 
-        cur.action["STATECHANGE"].append({})
-        cur.action["STATECHANGE"][-1]["object"] = object
-        cur.action["STATECHANGE"][-1]["to"] = to
+        self.cur.action["STATECHANGE"].append({})
+        self.cur.action["STATECHANGE"][-1]["object"] = object
+        self.cur.action["STATECHANGE"][-1]["to"] = to
 
+    def updateACT(self, type: str, key: str, val: str, index=None):
+        # get the timestep that needs to be updated
+        if not index:
+            node = self.cur
+        else:
+            node = self.get(index)
 
-# testing
-model = Mental_model(Containment(), Space(), Touching())
-model.PTRANS("rain", to="ground", From="sky")
-model.PTRANS("elephant", to="home", From="zoo")
-model.PTRANS("bus", to="stop", From="school")
+        # update the last ACT of that type
+        node.action[type][-1][key] = val
 
-model.advance_time()
-model.PSTOP("bus")
-model.PSTOP("elephant")
-
-model.advance_time()
-model.print(0)
-model.print(1)
-model.print(2)
+# # testing
+# model = Mental_model()
+# model.PTRANS("elephant", to="home", From="zoo")
+# model.PTRANS("bus", to="stop", From="school")
+#
+# model.advance_time()
+# model.PTRANS("rain", to="ground", From="sky")
+# model.PSTOP("bus")
+# model.PSTOP("elephant")
+#
+# model.advance_time()
+# model.updateACT("PTRANS", "from", "south")
+# model.print(1)
+# model.print(2)
+# model.print(3)
+# model.print_latest()
